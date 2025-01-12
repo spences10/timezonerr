62a7e93d0a85b25429eb87ba38778411bf16865c
spences10 <spences10apps@gmail.com>
2025-01-12 15:16:47 +0000

    refactor: :recycle: update timezone visualization to display current times instead of timezones, enhancing the component's functionality

--- src/lib/components/timezone-visualization.svelte
+++ src/lib/components/timezone-visualization.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { get_timezones } from '$lib/state/timezone.svelte';
+	import { get_current_times } from '$lib/state/timezone.svelte';
 	import TimeZoneRow from './timezone-row.svelte';
 
-	let timezones = $derived(get_timezones());
+	let current_times = $derived(get_current_times());
 </script>
 
 <div class="card bg-base-100 px-5 pt-6 shadow-xl">
 	<div class="overflow-x-auto">
 		<div class="min-w-full">
-			{#each timezones as { city, timezone }}
-				<TimeZoneRow {city} {timezone} />
+			{#each current_times as { city, timezone, time }}
+				<TimeZoneRow {city} {timezone} {time} />
 			{/each}
 		</div>
 	</div>
