01e244f821b41657d1152a0871226dabe92c2854
spences10 <spences10apps@gmail.com>
2025-01-12 12:23:26 +0000

    feat: :sparkles: integrate Fathom analytics for page tracking in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import { page } from '$app/state';
+	import {
+		PUBLIC_FATHOM_ID,
+		PUBLIC_FATHOM_URL,
+	} from '$env/static/public';
+	import * as Fathom from 'fathom-client';
 	import '../app.css';
 
 	let { children } = $props();
+
+	$effect(() => {
+		if (browser) {
+			Fathom.load(PUBLIC_FATHOM_ID, {
+				url: PUBLIC_FATHOM_URL,
+			});
+		}
+	});
+
+	// Track pageview on route change
+	$effect(() => {
+		page.url.pathname, browser && Fathom.trackPageview();
+	});
 </script>
 
 <div class="min-h-screen bg-base-200">
